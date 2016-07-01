75a2708ade0af03e38a91c67c0fee8237b935e1a
Adib Rastegarnia <arastega@purdue.edu>
2016-06-30 21:00:12 -0400

    source files formatted

--- xinu-node/wsnode/wsnode.c
+++ xinu-node/wsnode/wsnode.c
@@ -14,15 +14,15 @@ struct node_info info;
 /*-----------------------------------------------------------
 * Create and Ethernet packet and fill out header fields
 *----------------------------------------------------------*/
-struct etherPkt *create_etherPkt(struct netpacket *pkt)
+struct etherPkt *create_etherPkt ( struct netpacket *pkt )
 {
     struct etherPkt *msg;
-    msg= (struct etherPkt *)getmem(sizeof(struct etherPkt));
+    msg = ( struct etherPkt * ) getmem ( sizeof ( struct etherPkt ) );
     /*fill out Ethernet packet header fields */
-    memset(msg, 0, sizeof(msg));
-    memcpy(msg->src, NetData.ethucast, ETH_ADDR_LEN);
-    memcpy(msg->dst, pkt->net_ethsrc, ETH_ADDR_LEN);
-    msg->type = htons(ETH_TYPE_A);
+    memset ( msg, 0, sizeof ( msg ) );
+    memcpy ( msg->src, NetData.ethucast, ETH_ADDR_LEN );
+    memcpy ( msg->dst, pkt->net_ethsrc, ETH_ADDR_LEN );
+    msg->type = htons ( ETH_TYPE_A );
     return msg;
 }
 /*------------------------------------------------
@@ -32,18 +32,18 @@ status wsnode_join()
 {
     struct etherPkt *join_msg;
     int32 retval;
-    join_msg= (struct etherPkt *)getmem(sizeof(struct etherPkt));
+    join_msg = ( struct etherPkt * ) getmem ( sizeof ( struct etherPkt ) );
     /*fill out Ethernet packet header fields */
-    memset(join_msg, 0, sizeof(join_msg));
-    memcpy(join_msg->src, NetData.ethucast, ETH_ADDR_LEN);
-    memcpy(join_msg->dst, NetData.ethbcast, ETH_ADDR_LEN);
-    join_msg->type = htons(ETH_TYPE_A);
+    memset ( join_msg, 0, sizeof ( join_msg ) );
+    memcpy ( join_msg->src, NetData.ethucast, ETH_ADDR_LEN );
+    memcpy ( join_msg->dst, NetData.ethbcast, ETH_ADDR_LEN );
+    join_msg->type = htons ( ETH_TYPE_A );
     /*fill out Ethernet packet data fields */
-    join_msg->amsgtyp = htonl(A_JOIN);
-    join_msg->anodeid = htons(0);
-    retval = write(ETHER0, (char *)join_msg, sizeof(struct etherPkt));
+    join_msg->amsgtyp = htonl ( A_JOIN );
+    join_msg->anodeid = htons ( 0 );
+    retval = write ( ETHER0, ( char * ) join_msg, sizeof ( struct etherPkt ) );
 
-    if(retval > 0)
+    if ( retval > 0 )
         return OK;
 
     else
@@ -52,24 +52,24 @@ status wsnode_join()
 /*-------------------------------------------------------------
  * Send ack message as a repsonse of assign and ping messages
  * ----------------------------------------------------------*/
-status wsnode_sendack(struct netpacket *pkt)
+status wsnode_sendack ( struct netpacket *pkt )
 {
     struct etherPkt *ack_msg;
     struct etherPkt *node_msg;
-    node_msg = (struct etherPkt *)pkt;
-    ack_msg = create_etherPkt(pkt);
+    node_msg = ( struct etherPkt * ) pkt;
+    ack_msg = create_etherPkt ( pkt );
     int32 retval;
-    ack_msg->amsgtyp = htonl(A_ACK);
-    ack_msg->anodeid = htonl(info.nodeid);
-    memcpy(ack_msg->aacking,(char *)(node_msg) + 14, 16);
+    ack_msg->amsgtyp = htonl ( A_ACK );
+    ack_msg->anodeid = htonl ( info.nodeid );
+    memcpy ( ack_msg->aacking, ( char * ) ( node_msg ) + 14, 16 );
     //int i;
     /*for (i=0; i<16; i++)
     {
         //kprintf("aacking: %d\n", ack_msg->aacking[i]);
     }*/
-    retval = write(ETHER0, (char *)ack_msg, sizeof(struct etherPkt));
+    retval = write ( ETHER0, ( char * ) ack_msg, sizeof ( struct etherPkt ) );
 
-    if(retval > 0)
+    if ( retval > 0 )
         return OK;
 
     else
@@ -80,11 +80,11 @@ status wsnode_sendack(struct netpacket *pkt)
 -----------------------------------------------------------------*/
 void print_info()
 {
-    int i,j;
-    kprintf("node id:%d\n", info.nodeid);
+    int i, j;
+    kprintf ( "node id:%d\n", info.nodeid );
 
-    for (j=0; j< 6; j++) {
-        for (i=7; i>=0; i--) {
+    for ( j = 0; j < 6; j++ ) {
+        for ( i = 7; i >= 0; i-- ) {
             // kprintf("%d ", (info.mcastaddr[j]>> i) &0x01);
         }
 
@@ -97,57 +97,57 @@ void print_info()
  * Message handler is used to call
  * appropiate function based on message type.
  * -----------------------------------------------*/
-void amsg_handler(struct netpacket *pkt)
+void amsg_handler ( struct netpacket *pkt )
 {
     float delay;
     struct etherPkt *node_msg;
-    node_msg = (struct etherPkt *)pkt;
-    int32 amsgtyp = ntohl(node_msg->amsgtyp);
+    node_msg = ( struct etherPkt * ) pkt;
+    int32 amsgtyp = ntohl ( node_msg->amsgtyp );
     int i;
 
-    switch(amsgtyp) {
+    switch ( amsgtyp ) {
     case A_ASSIGN:
-        info.nodeid = ntohl(node_msg->anodeid);
+        info.nodeid = ntohl ( node_msg->anodeid );
 
-        if(wsnode_sendack(pkt)== OK) {
-            for (i=0; i< 6; i++) {
+        if ( wsnode_sendack ( pkt ) == OK ) {
+            for ( i = 0; i < 6; i++ ) {
                 info.mcastaddr[i] = node_msg->amcastaddr[i];
             }
 
             print_info();
-            kprintf("\n====>ACK message is sent\n");
+            kprintf ( "\n====>ACK message is sent\n" );
         }
 
         break;
 
     case A_RESTART:
-        kprintf("<==== RESTART message is received\n");
+        kprintf ( "<==== RESTART message is received\n" );
         break;
 
     case A_XOFF:
-        kprintf("<====XOF message is received\n");
+        kprintf ( "<====XOF message is received\n" );
         break;
 
     case A_XON:
-        kprintf("<==== XON message is received\n");
+        kprintf ( "<==== XON message is received\n" );
         break;
 
     case A_PING:
-        kprintf("<==== PING message is received\n");
+        kprintf ( "<==== PING message is received\n" );
 
-        if(wsnode_sendack(pkt) == OK) {
-            kprintf("====> ACK message is sent\n");
+        if ( wsnode_sendack ( pkt ) == OK ) {
+            kprintf ( "====> ACK message is sent\n" );
         }
 
         break;
 
     case A_PING_ALL:
-        delay = (info.nodeid * 0.001);
-        sleep(delay);
-        kprintf("<==== PINGALL message is received\n");
+        delay = ( info.nodeid * 0.001 );
+        sleep ( delay );
+        kprintf ( "<==== PINGALL message is received\n" );
 
-        if(wsnode_sendack(pkt) == OK) {
-            kprintf("====> ACK message is sent\n");
+        if ( wsnode_sendack ( pkt ) == OK ) {
+            kprintf ( "====> ACK message is sent\n" );
         }
 
         break;
--- xinu-server/wsserver/wstopology.c
+++ xinu-server/wsserver/wstopology.c
@@ -9,45 +9,45 @@
  *      int nodes;
  *      int status = read_topology("filename", &buff, &nodes);
  ***********************************************************************/
-status read_topology(char *name, char **buff, uint32 *size)
+status read_topology ( char *name, char **buff, uint32 *size )
 {
     /* Open topology file from server */
-    int32 fd = open(RFILESYS, name, "ro");
+    int32 fd = open ( RFILESYS, name, "ro" );
 
-    if (fd == SYSERR) {
-        printf("WARNING: Could not open topology file for reading\n");
+    if ( fd == SYSERR ) {
+        printf ( "WARNING: Could not open topology file for reading\n" );
         return SYSERR;
     }
 
     /* Get size of topology file so we know how much to read */
-    *size = control(RFILESYS, RFS_CTL_SIZE, 0, 0);
+    *size = control ( RFILESYS, RFS_CTL_SIZE, 0, 0 );
 
-    if (*size == SYSERR) {
-        printf("WARNING: Could not get topology file size\n");
-        close(fd);
+    if ( *size == SYSERR ) {
+        printf ( "WARNING: Could not get topology file size\n" );
+        close ( fd );
         return SYSERR;
     }
 
     /* Allocate buffer so that caller knows where it is */
-    *buff = getmem(*size);
+    *buff = getmem ( *size );
 
-    if ((int32) *buff == SYSERR) {
-        printf("WARNING: Could not allocate memory for topology\n");
-        close(fd);
+    if ( ( int32 ) *buff == SYSERR ) {
+        printf ( "WARNING: Could not allocate memory for topology\n" );
+        close ( fd );
         return SYSERR;
     }
 
     /* Read the topology file into the buffer */
-    int32 status = read(fd, *buff, *size);
+    int32 status = read ( fd, *buff, *size );
 
-    if (status == SYSERR) {
-        printf("WARNING: Could not read topology file contents\n");
-        close(fd);
-        freemem(*buff,*size);
+    if ( status == SYSERR ) {
+        printf ( "WARNING: Could not read topology file contents\n" );
+        close ( fd );
+        freemem ( *buff, *size );
         return SYSERR;
     }
 
-    close(fd);
+    close ( fd );
     return OK;
 }
 
@@ -55,7 +55,7 @@ status read_topology(char *name, char **buff, uint32 *size)
  * print_raw_topology: Print MAC addresses in a binary topology
  *  (this is mostly useful for testing)
  ***********************************************************************/
-int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
+int32 topo_update ( char *buff, uint32 size, struct t_entry *topo )
 {
     int i, j;
     int nnodes = 46;
@@ -66,9 +66,9 @@ int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
     int name_size;
     i = 0;
 
-    while (counter <= size) {
-        if (flag != -1) {
-            for(j = 0; j < ETH_ADDR_LEN; j++) {
+    while ( counter <= size ) {
+        if ( flag != -1 ) {
+            for ( j = 0; j < ETH_ADDR_LEN; j++ ) {
                 /*
                        for (k=7; k>=0; k--)
                        {
@@ -76,12 +76,12 @@ int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
                        }
                        kprintf(" ");
                 */
-                if ((int)buff[i * ETH_ADDR_LEN +j] == 0)
+                if ( ( int ) buff[i * ETH_ADDR_LEN + j] == 0 )
                     flag++;
             }
 
             //kprintf("\n");
-            if (flag == 6)
+            if ( flag == 6 )
                 flag = -1;
 
             else {
@@ -89,8 +89,8 @@ int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
                 topo[i].t_nodeid = i;
                 topo[i].t_status = 0;
 
-                for (j=0; j<6; j++) {
-                    topo[i].t_neighbors[j] = (unsigned char)buff[counter + j];
+                for ( j = 0; j < 6; j++ ) {
+                    topo[i].t_neighbors[j] = ( unsigned char ) buff[counter + j];
                 }
 
                 i++;
@@ -99,14 +99,14 @@ int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
             counter = counter + 6;
 
         } else {
-            if (len_flag == 0) {
+            if ( len_flag == 0 ) {
                 name_size = buff[counter];
                 counter++;
                 len_flag = 1;
 
             } else {
                 //int name[name_size];
-                for (j=0; j<name_size; j++) {
+                for ( j = 0; j < name_size; j++ ) {
                     //name[j] = buff[counter + j];
                     //kprintf("%s",&name[j]);
                 }
@@ -120,7 +120,7 @@ int32 topo_update(char *buff, uint32 size, struct t_entry *topo)
 
     num_of_entries = i;
 
-    for (i= num_of_entries -1; i<nnodes; i++) {
+    for ( i = num_of_entries - 1; i < nnodes; i++ ) {
         topo[i].t_nodeid = i;
         topo[i].t_status = 0;
     }
