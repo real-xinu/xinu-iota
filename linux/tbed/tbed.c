5512f68b280f17944a7db086113e1d1ef56989b7
Adib Rastegarnia <arastega@purdue.edu>
2016-07-03 23:30:43 -0400

    The source codes formated

--- linux/tbed/tbed.c
+++ linux/tbed/tbed.c
@@ -169,7 +169,6 @@ int ts_find()
     char *recvbuf;
     recvbuf = malloc (sizeof (struct c_msg));
     struct sockaddr_in  si_other;
-    //int s;
     socklen_t slen = sizeof (si_other);
     struct c_msg *buf = malloc (sizeof (struct c_msg));
     struct c_msg message;
@@ -177,21 +176,6 @@ int ts_find()
     tv.tv_sec = 0;
     tv.tv_usec = TIME_OUT_TS;
     char list_ip[15][46];
-    //if ((s = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-    //    error_handler ("socket");
-    // }
-    /*memset ((char *)&si_me, 0, sizeof (si_me));
-    //si_me.sin_family = AF_INET;
-    si_me.sin_port = htons (PORT);
-    si_me.sin_addr.s_addr = htonl (INADDR_ANY);
-    int enable = 1;
-
-    if (setsockopt (s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof (int)) < 0)
-        error_handler ("setsockopt(SO_REUSEADDR) failed");
-
-    if ( bind (s , (struct sockaddr*)&si_me, sizeof (si_me) ) == -1) {
-        error_handler ("Sock can not bind to the address");
-    }*/
     si_other.sin_family = AF_INET;
     si_other.sin_port = htons (PORT);
 
