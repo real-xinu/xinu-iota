29725ff508ba84ce6518e72baa6da2b24d37731c
Adib Rastegarnia <arastega@purdue.edu>
2016-07-21 13:21:45 -0400

    cmdhandler.c is added

---
+++ linux/tbed/include/global.h
@@ -0,0 +1,17 @@
+#ifndef  GLOBAL_H
+#define  GLOBAL_H
+
+extern int s;
+extern FILE *fp;
+extern char *path;;
+extern char *imgpath;
+extern struct sockaddr_in si_other;
+extern const char *SRV_IP;
+extern char map_list[46][100];
+extern char map_serv[15];
+extern char ip_serv[15];
+extern char map_brouter[15];
+extern const char * const  cmdtab[];
+extern int ncmd; 
+#endif
+
---
+++ linux/tbed/include/macros.h
@@ -0,0 +1,26 @@
+#define IP_BRDCAST   "128.10.137.255"
+#define BUFLEN 2048
+#define PORT 55000  // UDP prort
+#define ERR -1
+#define OK 1
+#define BIT_TEST 1
+#define BIT_SET 0
+#define TIME_OUT 10
+#define TIME_OUT_TS 1000
+#define ALL -1     // This macro is used for ping all
+// toptology status variables
+#define ALIVE 1
+#define NOTRESP -1
+#define NOTACTIV 0
+
+//cs-console commands
+#define DOWNLOAD "DOWNLOAD"
+#define POWERCYCLE "POWERCYCLE"
+#define EXEC "cs-console"
+#define SERVERIMG "xinu.boot.s"
+#define NODEIMG   "xinu.boot.n"
+#define XINUSERVER "xinuserver.cs.purdue.edu"
+#define SERVER "beagle183"
+#define NETIP   "128.10.137."
+
+
